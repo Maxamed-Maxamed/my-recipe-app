07a925039c76f833fbb5e654b5b6ddb5e871cd3e
Maxamed-Maxamed <20088349@mail.wit.ie>
2024-08-29 16:56:17 +0100

    Refactoriser App.js et global.css : réorganiser la structure des routes, supprimer le code redondant et mettre à jour les styles CSS fix: Ensure footer stays at the bottom of the page and adjust main layout structure

--- src/App.js
+++ src/App.js
@@ -5,41 +5,35 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import RecipeDetailPage from './pages/RecipeDetailPage';
 import Footer from './components/Footer';
-
-
-
 import Login from './components/Login';
 import SignUp from './components/SignUp';
-// ... existing code ...
 import ManageRecipesPage from './pages/ManageRecipesPage';
 import FavoritesPage from './pages/FavoritesPage';
 import CategoriesPage from './pages/CategoriesPage';
 import CategoryRecipesPage from './pages/CategoryRecipesPage';
 import PrivateRoute from './components/PrivateRoute';
-
 import './styles/App.css';
 
 function App() {
   return (
     <Router>
-      <Header />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/recipe/:id" element={<RecipeDetailPage />} />
-
-        <Route path="/signup" element={< SignUp />} />
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/manage-recipes" element={<PrivateRoute><ManageRecipesPage /></PrivateRoute>} />
-        <Route path="/favorites" element={<PrivateRoute><FavoritesPage /></PrivateRoute>} />
-        <Route path="/categories" element={<CategoriesPage />} />
-        <Route path="/category/:category" element={<CategoryRecipesPage />} />
-
-      </Routes>
-      <Footer />
+      <div className="app-container">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/recipe/:id" element={<RecipeDetailPage />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/manage-recipes" element={<PrivateRoute><ManageRecipesPage /></PrivateRoute>} />
+            <Route path="/favorites" element={<PrivateRoute><FavoritesPage /></PrivateRoute>} />
+            <Route path="/categories" element={<CategoriesPage />} />
+            <Route path="/category/:category" element={<CategoryRecipesPage />} />
+          </Routes>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,78 +1,39 @@
-/**
-
- * The `Header` function in this code snippet generates a navigation bar for a Recipe App with links to
-
- * different pages and authentication options.
- * @returns The `Header` component is being returned, which contains the AppBar with a Toolbar and
- * various Buttons for navigation and user authentication. The appearance and functionality of the
-
- * header will depend on whether a user is authenticated or not.
-
- */
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
-
 import '../styles/App.css'; // Ensure the styles are imported
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../firebaseConfig';
 
 function Header() {
-
   const [user] = useAuthState(auth);
 
-
   return (
-
     <AppBar position="static" style={{ backgroundColor: '#38A83F' }}>
-
       <Toolbar style={{ display: 'flex', justifyContent: 'space-between' }}>
-        <Typography variant="h6" style={{ flexGrow: 1, color: '#ffff' , fontFamily: 'Arial, sans-serif', background: '#38A83F'}}>
-
+        <Typography variant="h6" style={{ flexGrow: 1, color: '#ffff', fontFamily: 'Arial, sans-serif', background: '#38A83F' }}>
           The Chronicles of Carlson
         </Typography>
-
         <div>
-
           <Button color="inherit" component={Link} to="/" style={{ color: '#fff' }}>Home</Button>
-
           <Button color="inherit" component={Link} to="/about" style={{ color: '#fff' }}>About</Button>
-
-          <Button color="inherit" component={Link} to="/categories" style={{ color: '#fff' }}>Categories</Button>
-
-          <Button color="inherit" component={Link} to="/favorites" style={{ color: '#fff' }}>Favorites</Button>
-
           {user ? (
-
             <>
-
+              <Button color="inherit" component={Link} to="/categories" style={{ color: '#fff' }}>Categories</Button>
+              <Button color="inherit" component={Link} to="/favorites" style={{ color: '#fff' }}>Favorites</Button>
               <Button color="inherit" component={Link} to="/manage-recipes" style={{ color: '#fff' }}>Manage Recipes</Button>
-
               <Button color="inherit" onClick={() => auth.signOut()} style={{ color: '#fff' }}>Logout</Button>
-
             </>
-
           ) : (
-
             <>
-
               <Button color="inherit" component={Link} to="/login" style={{ color: '#fff' }}>Login</Button>
-
               <Button color="inherit" component={Link} to="/signup" style={{ color: '#fff' }}>Signup</Button>
-
             </>
-
           )}
-
         </div>
-
       </Toolbar>
-
     </AppBar>
-
   );
-
 }
-
 
 export default Header;
